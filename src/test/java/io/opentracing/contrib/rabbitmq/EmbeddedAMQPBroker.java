5a645758a8dceb65ada728ecb7fc596cf3f5e44f
Sergei Malafeev <sergeymalafeev@gmail.com>
2019-01-29 20:21:21 +0800

    upgrade amqp to 5.6.0
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- src/test/java/io/opentracing/contrib/rabbitmq/EmbeddedAMQPBroker.java
+++ src/test/java/io/opentracing/contrib/rabbitmq/EmbeddedAMQPBroker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 The OpenTracing Authors
+ * Copyright 2017-2019 The OpenTracing Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,29 +14,31 @@
 package io.opentracing.contrib.rabbitmq;
 
 
-import com.google.common.io.Files;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import javax.net.ServerSocketFactory;
-import org.apache.qpid.server.Broker;
-import org.apache.qpid.server.BrokerOptions;
+import org.apache.qpid.server.SystemLauncher;
 
 
 public class EmbeddedAMQPBroker {
 
   private int brokerPort;
-  private final Broker broker = new Broker();
+  private final SystemLauncher broker = new SystemLauncher();
 
   public EmbeddedAMQPBroker() throws Exception {
     this.brokerPort = findAvailableTcpPort();
     final String configFileName = "qpid-config.json";
-    final String passwordFileName = "passwd.properties";
-    final BrokerOptions brokerOptions = new BrokerOptions();
-    brokerOptions.setConfigProperty("qpid.amqp_port", String.valueOf(brokerPort));
-    brokerOptions.setConfigProperty("qpid.pass_file", findResourcePath(passwordFileName));
-    brokerOptions.setConfigProperty("qpid.work_dir", Files.createTempDir().getAbsolutePath());
-    brokerOptions.setInitialConfigurationLocation(findResourcePath(configFileName));
+    Map<String, Object> brokerOptions = new HashMap<>();
+    brokerOptions.put("type", "Memory");
+    Map<String, Object> context = new HashMap<>();
+    context.put("qpid.amqp_port", brokerPort);
+    context.put("qpid.work_dir", Files.createTempDirectory("qpid").toFile().getAbsolutePath());
+    brokerOptions.put("context", context);
+    brokerOptions.put("initialConfigurationLocation", findResourcePath(configFileName));
     // start broker
     broker.startup(brokerOptions);
   }
