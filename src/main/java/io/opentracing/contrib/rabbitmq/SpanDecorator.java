04aa69168701cdcc4b1847d09dc203a79e395e42
Sergei Malafeev <sergeymalafeev@gmail.com>
2020-03-02 21:27:09 +0800

    make SpanDecorator public
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- src/main/java/io/opentracing/contrib/rabbitmq/SpanDecorator.java
+++ src/main/java/io/opentracing/contrib/rabbitmq/SpanDecorator.java
@@ -17,16 +17,15 @@ package io.opentracing.contrib.rabbitmq;
 import io.opentracing.Span;
 import io.opentracing.tag.Tags;
 
-class SpanDecorator {
+public class SpanDecorator {
+  public static final String COMPONENT_NAME = "java-rabbitmq";
 
-  static final String COMPONENT_NAME = "java-rabbitmq";
-
-  static void onRequest(String exchange, Span span) {
+  public static void onRequest(String exchange, Span span) {
     Tags.COMPONENT.set(span, COMPONENT_NAME);
     Tags.MESSAGE_BUS_DESTINATION.set(span, exchange);
   }
 
-  static void onResponse(Span span) {
+  public static void onResponse(Span span) {
     Tags.COMPONENT.set(span, COMPONENT_NAME);
   }
 }
