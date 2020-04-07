48917338af44ec49cfb292a262d4e9f54ca12758
Sergei Malafeev <sergeymalafeev@gmail.com>
2020-04-07 23:00:40 +0800

    update copyright year
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- src/test/java/io/opentracing/contrib/rabbitmq/HeadersMapExtractAdapterTest.java
+++ src/test/java/io/opentracing/contrib/rabbitmq/HeadersMapExtractAdapterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 The OpenTracing Authors
+ * Copyright 2017-2020 The OpenTracing Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -13,66 +13,66 @@
  */
 package io.opentracing.contrib.rabbitmq;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashMap;
 import java.util.Map;
-
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 @RunWith(JUnitParamsRunner.class)
 public class HeadersMapExtractAdapterTest {
 
-    @Test
-    @Parameters(method = "emptyValues")
-    public void emptyHeadersMapExtract(Map<String, Object> inputMap) {
-        //given params
+  @Test
+  @Parameters(method = "emptyValues")
+  public void emptyHeadersMapExtract(Map<String, Object> inputMap) {
+    //given params
 
-        //when
-        HeadersMapExtractAdapter adapter = new HeadersMapExtractAdapter(inputMap);
+    //when
+    HeadersMapExtractAdapter adapter = new HeadersMapExtractAdapter(inputMap);
 
-        //then
-        assertFalse(adapter.iterator().hasNext());
-    }
+    //then
+    assertFalse(adapter.iterator().hasNext());
+  }
 
-    @Test
-    @Parameters(method = "nonEmptyValues")
-    public void nonEmptyHeadersMapExtract(String header, Object value, String expectedKey, String expectedValue) {
-        //given
-        Map<String, Object> inputMap = new HashMap<>();
-        inputMap.put(header, value);
+  @Test
+  @Parameters(method = "nonEmptyValues")
+  public void nonEmptyHeadersMapExtract(String header, Object value, String expectedKey,
+      String expectedValue) {
+    //given
+    Map<String, Object> inputMap = new HashMap<>();
+    inputMap.put(header, value);
 
-        //when
-        HeadersMapExtractAdapter adapter = new HeadersMapExtractAdapter(inputMap);
+    //when
+    HeadersMapExtractAdapter adapter = new HeadersMapExtractAdapter(inputMap);
 
-        //then
-        assertTrue(adapter.iterator().hasNext());
-        Map.Entry<String, String> entry = adapter.iterator().next();
-        assertEquals(expectedValue, entry.getValue());
-        assertEquals(expectedKey, entry.getKey());
-    }
+    //then
+    assertTrue(adapter.iterator().hasNext());
+    Map.Entry<String, String> entry = adapter.iterator().next();
+    assertEquals(expectedValue, entry.getValue());
+    assertEquals(expectedKey, entry.getKey());
+  }
 
-    @Parameters
-    private Object[] emptyValues() {
-        return new Object[]{
-                new Object[]{null},
-                new Object[]{new HashMap<>()},
-        };
-    }
+  @Parameters
+  private Object[] emptyValues() {
+    return new Object[]{
+        new Object[]{null},
+        new Object[]{new HashMap<>()},
+    };
+  }
 
-    @Parameters
-    private Object[] nonEmptyValues() {
-        return new Object[]{
-                new Object[]{"header1", "value1",  "header1", "value1"},
-                new Object[]{"header2", "",        "header2", ""},
-                new Object[]{"header3", null,      "header3", "null"},
-                new Object[]{"header4", 1L,        "header4", "1"},
-                new Object[]{"header5", false,     "header5", "false"},
-        };
-    }
+  @Parameters
+  private Object[] nonEmptyValues() {
+    return new Object[]{
+        new Object[]{"header1", "value1", "header1", "value1"},
+        new Object[]{"header2", "", "header2", ""},
+        new Object[]{"header3", null, "header3", "null"},
+        new Object[]{"header4", 1L, "header4", "1"},
+        new Object[]{"header5", false, "header5", "false"},
+    };
+  }
 }
--- src/test/java/io/opentracing/contrib/rabbitmq/TracingTest.java
+++ src/test/java/io/opentracing/contrib/rabbitmq/TracingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 The OpenTracing Authors
+ * Copyright 2017-2020 The OpenTracing Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -188,7 +188,8 @@ public class TracingTest {
     //just emulate a context is present
     Span span = mockTracer.buildSpan("test").start();
     try (Scope ignored = mockTracer.activateSpan(span)) {
-      Span child = TracingUtils.buildChildSpan(new AMQP.BasicProperties().builder().headers(headers).build(),
+      Span child = TracingUtils
+          .buildChildSpan(new AMQP.BasicProperties().builder().headers(headers).build(),
               "test", mockTracer);
       assertNotNull(child);
     }
